3e5ab6f0df9654e97e4f25a1400b17b9115f82b7
Justin <justinjenecke@gmail.com>
2024-01-14 22:12:54 +0200

    ceates demo console app + demo code till conditions

--- DemoConsoleApp/DemoConsoleApp/Program.cs
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -4,9 +4,32 @@ namespace DemoConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
+
+            int i = 139;
+            FunctionLibrary functionLibrary = new FunctionLibrary();
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(functionLibrary.TestFunction());
+
+            if (functionLibrary.TestFunction2(i)) {
+                Console.WriteLine(i + " is smaller than 100");
+            } else {
+                Console.WriteLine(i + " is greater than 100");
+            }
+
+            Console.ReadKey();
         }
+    }
+
+    class FunctionLibrary {
+
+        public string TestFunction() {
+            return "This is a test";
+        }
+        public bool TestFunction2(int x) {
+            return x < 100;
+        }
+
     }
 }
